1b2ced3a986ac6a5e3c1ac3d16e041dd4e9f219c
Varun <varun@i2india.in>
2017-03-20 16:48:00 +0530

    added clevertap sync code

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,59 @@ import APP_VERSION from '../constants';
 // check for for the token, if not present make a request
 export default class App extends React.Component {
   
-  componentWillMount(){
-    this.props.appInit(APP_VERSION);
+   constructor(context,props){
+     
+     super(context,props);
+     
+     this.props.appInit(APP_VERSION);
+     
+     document.addEventListener('deviceready', this.onDeviceReady, false);
+     // document.addEventListener('onCleverTapProfileSync', this.onCleverTapProfileSync, false);
+     document.addEventListener('onCleverTapProfileDidInitialize', this.onCleverTapProfileDidInitialize, false);
+     // document.addEventListener('onCleverTapInAppNotificationDismissed', this.onCleverTapInAppNotificationDismissed, false);
+     // deeplink handler
+     // document.addEventListener('onDeepLink', this.onDeepLink, false);
+     //push notification handler
+     document.addEventListener('onPushNotification', this.onPushNotification, false);
   }
 
+  onDeviceReady(){
+    CleverTap.notifyDeviceReady();
+    CleverTap.registerPush();
+    CleverTap.enablePersonalization();
+  }
+
+  onCleverTapProfileSync(e) {
+        console.log(e.updates);
+    }
+    
+  onCleverTapProfileDidInitialize(e) {
+        
+        CleverTap.profileSet({"Email" : "varun@i2india.in","Identity":"1854"});
+        CleverTap.recordEventWithName("varun");
+        // CleverTap.recordEventWithNameAndProps("boo", {"bar":"zoo"});
+        console.log(e.CleverTapID);
+    }
+    
+  onCleverTapInAppNotificationDismissed(e){
+        console.log(e.extras);
+        console.log(e.actionExtras);
+    }
+    
+    // deep link handling
+    onDeepLink(e) {
+        console.log(e.deeplink);
+    }
+    
+    // push notification payload handling
+    onPushNotification(e) {
+        
+        //capture the intent of the push notification
+        //if single article then open the view of a single article
+        // alert(e.notification);
+        console.log(e.notification);
+  }
+  
   //replace this with props
   renderPage(route, navigator) {
     const props = route.props || {};
